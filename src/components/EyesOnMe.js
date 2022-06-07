526f78e6913cc7907ccd3fee331de1236e90afe2
kiprotichdominic <kiprotichdominic102@gmail.com>
2022-06-08 00:34:51 +0300

    Done with lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    function buttonFocus(event) {
+        console.log("Good!");
+    }
+    function buttonBlur(event) {
+        console.log("Hey! Eyes on me!");
+    }
+    return (
+        <div>
+            <button onFocus={buttonFocus} onBlur={buttonBlur}>
+                Eyes on me
+            </button>
+        </div>
+    )
+}
+
+export default EyesOnMe
